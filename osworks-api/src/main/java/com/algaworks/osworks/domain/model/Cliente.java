0b4aef3206970689b9483c811127ee5695f795cf
Felipe Santos <felipe.santos11@hotmail.com>
2020-04-25 16:06:00 -0300

    crud da api e validações de dados na classe Cliente

--- osworks-api/src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ osworks-api/src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -1,13 +1,35 @@
 package com.algaworks.osworks.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
 	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
 	
+	@NotBlank
+	@Size(max = 20)
+	@Column(name = "fone")
 	private String telefone;
 
 	public Long getId() {
@@ -40,6 +62,31 @@ public class Cliente {
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
